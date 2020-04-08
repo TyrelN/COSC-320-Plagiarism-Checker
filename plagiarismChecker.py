ef83604149541245debc3f7cbc1bd87e9492b285
mint-key <57965664+mint-key@users.noreply.github.com>
2020-04-07 20:38:24 -0700

    Update plagiarismChecker.py

--- plagiarismChecker.py
+++ plagiarismChecker.py
@@ -64,7 +64,28 @@ def lcs(X, Y, m, n):
     else: 
        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));                 
 
+def lcs(X, Y):
+    # find the length of the strings 
+    m = len(X)
+    n = len(Y)
 
+    # declaring the array for storing the dp values 
+    L = [[None] * (n + 1) for i in xrange(m + 1)]
+
+    """Following steps build L[m+1][n+1] in bottom up fashion 
+    Note: L[i][j] contains length of LCS of X[0..i-1] 
+    and Y[0..j-1]"""
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif X[i - 1] == Y[j - 1]:
+                L[i][j] = L[i - 1][j - 1] + 1
+            else:
+                L[i][j] = max(L[i - 1][j], L[i][j - 1])
+
+                # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1] 
+    return L[m][n]
 
 #Now the Rabin-Karp Algorithm, which checks for entire words that match:
 #https://www.geeksforgeeks.org/rabin-karp-algorithm-for-pattern-searching/
@@ -103,7 +124,7 @@ def RBKCheck(pat, txt, q):# q must be a prime number
 
 print("here is the current directory: " + os.getcwd())
 
-file1 = open("./example.txt", "r")
+file1 = open("example.txt", "r")
 
 text = file1.read() #this text file now holds the entire string from example.txt
 
